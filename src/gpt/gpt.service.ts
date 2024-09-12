0edc0824df97b83bd73f2417540a88088f14dff8
alemoles <alemolerio@gmail.com>
2024-09-11 22:19:43 -0300

    create service in service

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class GptService {}
+export class GptService {
+    // Invoke use cases only
+    orthographyCheck() {
+        return {
+            hola: 'Mundo desde orthographyCheck'
+        };
+    }
+}
