58467e752a394a311264ed52aea170d50bdf4f9d
alemoles <alemolerio@gmail.com>
2024-09-18 21:36:52 -0300

    create first prompt request

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -6,8 +6,21 @@ interface Options {
 
 export const orthographyCheckUseCase = async (openai: OpenAI, options: Options) => {
     const { prompt } = options;
-    return {
-        prompt: prompt,
-        apikey: process.env.OPENAI_API_KEY
-    };
+    const completion = await openai.chat.completions.create({
+        messages: [{
+            role: "system",
+            content: "You are a helpful assistant."
+        }],
+        model: "gpt-4o",
+        temperature: 0.3,
+    });
+
+    console.log({ completion });
+    return completion.choices[0];
+
+
+    // return {
+    //     prompt: prompt,
+    //     apikey: process.env.OPENAI_API_KEY
+    // };
 }
