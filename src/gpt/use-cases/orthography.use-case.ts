8f3cb4e8a560d2908e87ef137549e1f044224a49
alemoles <alemolerio@gmail.com>
2024-09-18 21:39:08 -0300

    aadd user message to prompt request

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -7,10 +7,16 @@ interface Options {
 export const orthographyCheckUseCase = async (openai: OpenAI, options: Options) => {
     const { prompt } = options;
     const completion = await openai.chat.completions.create({
-        messages: [{
-            role: "system",
-            content: "You are a helpful assistant."
-        }],
+        messages: [
+            {
+                role: "system",
+                content: "You are a helpful assistant."
+            },
+            {
+                role: "user",
+                content: prompt
+            }
+        ],
         model: "gpt-4o",
         temperature: 0.3,
     });
