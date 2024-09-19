cc0d31e2b80124b1222944dd7061d83ddfeeb015
alemoles <alemolerio@gmail.com>
2024-09-18 21:24:29 -0300

    add open ai package

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { orthographyCheckUseCase } from './use-cases';
 import { OrthographyDto } from './dtos';
+import OpenAI from 'openai';
 
 @Injectable()
 export class GptService {
+    private openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+
     // Invoke use cases only
     async orthographyCheck(orthographyDto: OrthographyDto) {
         return await orthographyCheckUseCase({ prompt: orthographyDto.prompt });
