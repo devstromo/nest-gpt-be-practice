e36eabeef747e17bbb75b5f8c55b934ed9f220ec
alemoles <alemolerio@gmail.com>
2024-10-04 20:27:44 -0300

    create translate use case

--- src/gpt/use-cases/index.ts
+++ src/gpt/use-cases/index.ts
@@ -1,3 +1,4 @@
 export * from './orthography.use-case';
 export * from './pros-cons-discusser.user-case';
 export * from './pros-cons-discusser-stream.user-case';
+export * from './translate.user-case';
