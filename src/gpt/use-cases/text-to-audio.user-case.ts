0945ee4603a7f619b849f93c0b041a281be37a93
alemoles <alemolerio@gmail.com>
2024-10-12 00:41:23 -0300

    generate audio file in use case

--- src/gpt/use-cases/text-to-audio.user-case.ts
+++ src/gpt/use-cases/text-to-audio.user-case.ts
@@ -1,4 +1,6 @@
 import OpenAI from "openai";
+import * as path from "path";
+import * as fs from "fs";
 
 interface Options {
     prompt: string;
@@ -9,12 +11,25 @@ export const textToAudioUseCase = async (openai: OpenAI, { prompt, voice }: Opti
     const voices = {
         'nova': 'nova',
         'alloy': 'alloy',
+        'echo': 'echo',
+        'fable': 'fable',
+        'onyx': 'onyx',
+        'shimmer': 'shimmer'
     }
     const selectedVoice = voices[voice] ?? 'nova';
 
+    const folder = path.resolve(__dirname, `../../../generated/audio/`);
+    const speechFile = path.resolve(`${folder}/${new Date().getTime()}.mp3`);
 
-    return {
-        prompt: prompt,
-        selectedVoice: selectedVoice
-    };
+    fs.mkdirSync(folder, { recursive: true });
+    const mp3 = await openai.audio.speech.create({
+        model: 'tts-1',
+        voice: selectedVoice,
+        input: prompt,
+        response_format: 'mp3',
+    });
+
+    const buffer = Buffer.from(await mp3.arrayBuffer());
+    fs.writeFileSync(speechFile, buffer);
+    return speechFile;
 }
