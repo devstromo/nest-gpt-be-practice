d60496bb0d29ee11faac99a2bfd27837d26eaa73
alemoles <alemolerio@gmail.com>
2024-10-25 23:20:18 -0300

    update image generation use case
    - image generation feature

--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -7,9 +7,25 @@ interface Options {
     maskImage?: string;
 }
 
-export const imageGenerationUseCase = async (openAI: OpenAI, options: Options) => {
+export const imageGenerationUseCase = async (openai: OpenAI, options: Options) => {
     const { prompt, originalImage, maskImage } = options
-    console.log({ prompt, originalImage, maskImage });
 
+    //Todo: Check original image
+    const response = await openai.images.generate({
+        prompt: prompt,
+        model: 'dall-e-3',
+        n: 1,
+        size: '1024x1024',
+        quality: 'standard',
+        response_format: 'url'
+    });
+
+    // Todo: save image in FS
+    console.log(response);
+    return {
+        url: response.data[0].url,
+        localPath: '',
+        revised_response: response.data[0].revised_prompt
+    }
 
 }
