29fc925d2d6297746ffc59fc4523e8273b131ce3
alemoles <alemolerio@gmail.com>
2024-11-01 23:52:58 -0300

    implement image variation use case

--- src/gpt/use-cases/image-variation.use-case.ts
+++ src/gpt/use-cases/image-variation.use-case.ts
@@ -1,8 +1,7 @@
 import * as fs from 'fs';
-import * as path from 'path';
 
 import OpenAI from 'openai';
-import { downloadBase64ImageAsPng, downloadImageAsPng } from 'src/helpers';
+import { downloadImageAsPng } from 'src/helpers';
 
 
 interface Options {
@@ -11,6 +10,21 @@ interface Options {
 
 export const imageVariationUseCase = async (openai: OpenAI, options: Options) => {
     const { baseImage } = options
+    const pngImagePath = await downloadImageAsPng(baseImage, true);
 
-    return baseImage;
+    const response = await openai.images.createVariation({
+        model: 'dall-e-2',
+        image: fs.createReadStream(pngImagePath),
+        n: 1,
+        size: '1024x1024',
+        response_format: 'url',
+    });
+
+    const fileName = await downloadImageAsPng(response.data[0].url)
+    const url = `${process.env.SERVER_URL}/gpt/image-generation/${fileName}`
+    return {
+        url: url,
+        openAIUrl: response.data[0].url,
+        revised_response: response.data[0].revised_prompt
+    };
 };
