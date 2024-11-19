0d5a1e0f3e9b41375a2375490fc784de2b8a83e1
alemoles <alemolerio@gmail.com>
2024-11-18 22:15:55 -0300

    create endpoints in assistant controller

--- src/sam-assistant/sam-assistant.controller.ts
+++ src/sam-assistant/sam-assistant.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { SamAssistantService } from './sam-assistant.service';
+import { ImageGenerationDto } from 'src/gpt/dtos';
+import { QuestionDto } from './dtos/question.dto';
 
 @Controller('sam-assistant')
 export class SamAssistantController {
-  constructor(private readonly samAssistantService: SamAssistantService) {}
+  constructor(private readonly samAssistantService: SamAssistantService) { }
+
+  @Post('create-thread')
+  async createThread() {
+    return 'Create thread';
+  }
+
+  @Post('user-thread')
+  async userQuestion(
+    @Body() quesitonDto: QuestionDto
+  ) {
+    return quesitonDto;
+  }
 }
