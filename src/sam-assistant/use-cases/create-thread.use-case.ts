908772b2f6544f2f4da12f9f16d3bc48b2eebac5
alemoles <alemolerio@gmail.com>
2024-11-22 19:35:14 -0300

    bind create thread use case

--- src/sam-assistant/use-cases/create-thread.use-case.ts
+++ src/sam-assistant/use-cases/create-thread.use-case.ts
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 
-export const CreateThreadUseCase = async (openai: OpenAI) => {
+export const createThreadUseCase = async (openai: OpenAI) => {
     const thread = await openai.beta.threads.create();
     console.log({ thread });
     return thread;
