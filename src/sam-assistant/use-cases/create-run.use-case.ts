d2f17d566757a8d89adc56a56ed2910a0a186702
alemoles <alemolerio@gmail.com>
2024-11-25 22:11:11 -0300

    create run use case

---
+++ src/sam-assistant/use-cases/create-run.use-case.ts
@@ -0,0 +1,16 @@
+import OpenAI from "openai";
+
+interface Options {
+    threadId: string;
+    assistantId?: string;
+}
+
+export const createRunUseCase = async (openai: OpenAI, options: Options) => {
+    const { threadId, assistantId = process.env.ASSITANT_KEY } = options;
+    const run = await openai.beta.threads.runs.create(threadId, {
+        assistant_id: assistantId,
+        // instructions: // Rewrite the assistant if instructions are provided.
+    });
+    console.log({ run });
+    return run
+}
